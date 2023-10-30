65542dbe67b07efe8d766cb2ebab2209e5143fff
= <nathaniel.risum2@gmail.com>
2023-10-30 13:47:03 +0100

    Implement abstract class MenuItem

--- BigMamma_InGroup/model/MenuItem.cs
+++ BigMamma_InGroup/model/MenuItem.cs
@@ -1,6 +1,17 @@
 ﻿namespace BigMamma_InGroup.model
 {
-    public class MenuItem
+    public abstract class MenuItem
     {
+        private string _name;
+        private int _price;
+        public MenuItem(string name, int price)
+        {
+            _name = name;
+            _price = price;
+        }
+
+        public string Name { get { return _name; } set { _name = value; } }
+        private int Price { get { return _price; } set { _price = value; } }
+
     }
 }
