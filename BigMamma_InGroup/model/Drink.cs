3ade7e886598f0169938d31e88b10632103732b1
= <nathaniel.risum2@gmail.com>
2023-10-30 14:08:54 +0100

    Implement Drink class including inheritance

--- BigMamma_InGroup/model/Drink.cs
+++ BigMamma_InGroup/model/Drink.cs
@@ -1,6 +1,21 @@
 ﻿namespace BigMamma_InGroup.model
 {
-    public class Drink
+    public class Drink : MenuItem
     {
+        private bool _containsAlcohol;
+        private bool _isVegan;
+        private bool _containssugar;
+        public Drink(string name, int price, bool containsAlcohol, bool isVegan, bool containsSugar) : base(name, price)
+        {
+             _containsAlcohol = containsAlcohol;
+            _isVegan = isVegan;
+            _containssugar = containsSugar;
+        }
+
+        public bool ContainsAlcohol { get { return _containsAlcohol; } set { _containsAlcohol = value; } }
+        public bool IsVegan { get { return _isVegan;} set { _isVegan = value;} }
+        public bool ContainsSugar { get { return _containssugar; } set { _containssugar = value; } }
+
+
     }
 }
