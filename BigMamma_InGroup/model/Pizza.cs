d48d34cdc9d5ff04d3360ec2df781cae2c04d8dc
Sigurd Refsbæk Poulsen <sip003@edu.zealand.dk>
2023-10-30 14:26:05 +0100

    Pizza Class

--- BigMamma_InGroup/model/Pizza.cs
+++ BigMamma_InGroup/model/Pizza.cs
@@ -2,5 +2,18 @@ namespace BigMamma_InGroup.model;
 
 public class Pizza : MenuItem
 {
-    
+    private bool _isVegan;
+    private bool _isDeeppan;
+    private bool _isClassic;
+
+    public Pizza(string name, int price, bool isVegan, bool isDeeppan, bool isClassic) : base(name, price)
+    {
+        _isVegan = isVegan;
+        _isDeeppan = isDeeppan;
+        _isClassic = isClassic;
+    }
+
+    public bool isVegan { get { return _isVegan; } set { _isVegan = value; } }
+    public bool isDeeppan { get { return _isDeeppan; } set { _isDeeppan = value; } }
+    public bool isClassic { get { return _isClassic; } set { _isClassic = value; } }
 }
