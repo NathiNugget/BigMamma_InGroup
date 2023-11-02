51e219cbdf0b4e7eb9dd8f1e5203fdcd80c72c07
Sigurd Refsbæk Poulsen <sip003@edu.zealand.dk>
2023-11-02 11:26:04 +0100

    Add Drinks Page

---
+++ BigMamma_InGroup/Pages/Menu/Drinks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigMamma_InGroup.Pages.Menu
+{
+    public class DrinksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
