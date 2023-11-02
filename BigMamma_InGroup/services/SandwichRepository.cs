116c59195952eb95a03299105540603173560e76
Søren Vinther Hansen <soh003@edu.zealand.dk>
2023-11-02 11:23:06 +0100

    Add Sandwich repository

--- BigMamma_InGroup/services/SandwichRepository.cs
+++ BigMamma_InGroup/services/SandwichRepository.cs
@@ -1,6 +1,25 @@
-﻿namespace BigMamma_InGroup.services
+﻿using BigMamma_InGroup.model;
+
+namespace BigMamma_InGroup.services
 {
     public class SandwichRepository
     {
+        private List<Sandwich> _sandwichList;
+
+        public SandwichRepository(bool test)
+        {
+            if(test)
+            {
+                _sandwichList = new List<Sandwich>();
+                _sandwichList.Add(new Sandwich("Pollo & Panchetta", 69, false));
+                _sandwichList.Add(new Sandwich("Skinke", 69, false));
+                _sandwichList.Add(new Sandwich("Mortadella", 69, false));
+                _sandwichList.Add(new Sandwich("Parma", 69, false));
+            }
+        }
+        public List<Sandwich> SandwichList
+        {
+            get { return _sandwichList; }
+        }
     }
 }
