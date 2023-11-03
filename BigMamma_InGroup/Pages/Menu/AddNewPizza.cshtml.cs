c72d7c5e9d5c59e5a6251d32fbc04eb3e0dc4831
= <nathaniel.risum2@gmail.com>
2023-11-03 10:32:30 +0100

    Add abillity to add Pizzas to pr plus add Menu to Index as well as showing numbers for pizzas

---
+++ BigMamma_InGroup/Pages/Menu/AddNewPizza.cshtml.cs
@@ -0,0 +1,35 @@
+using BigMamma_InGroup.model;
+using BigMamma_InGroup.services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigMamma_InGroup.Pages.Menu
+{
+    public class AddNewPizzaModel : PageModel
+    {
+        [BindProperty]
+        public string NewName { get; set; }
+        [BindProperty]
+        public int NewPrice { get; set; }
+
+        private PizzaRepository _pr; 
+
+        public AddNewPizzaModel(PizzaRepository pr)
+        {
+            _pr = pr;
+        }
+
+        public void OnGet()
+        {
+     
+        }
+
+        public IActionResult OnPost()
+        {
+            Pizza p = new Pizza(NewName, NewPrice, false, false, false, false);
+            _pr.PizzaList.Add(p);
+            return RedirectToPage("DisplayPizza");
+
+        }
+    }
+}
--- BigMamma_InGroup/services/SandwichRepository.cs
+++ BigMamma_InGroup/services/SandwichRepository.cs
@@ -11,10 +11,10 @@ namespace BigMamma_InGroup.services
             if(test)
             {
                 _sandwichList = new List<Sandwich>();
-                _sandwichList.Add(new Sandwich("Pollo & Panchetta", 69, false));
-                _sandwichList.Add(new Sandwich("Skinke", 69, false));
-                _sandwichList.Add(new Sandwich("Mortadella", 69, false));
-                _sandwichList.Add(new Sandwich("Parma", 69, false));
+                SandwichList.Add(new Sandwich("Pollo & Panchetta", 69, false));
+                SandwichList.Add(new Sandwich("Skinke", 69, false));
+                SandwichList.Add(new Sandwich("Mortadella", 69, false));
+                SandwichList.Add(new Sandwich("Parma", 69, false));
             }
         }
         public List<Sandwich> SandwichList
